39a79fb909c5b4218418d771423a6259eb2374e9
renananiceto <renan.a.ferreira94@gmail.com>
2021-05-22 18:04:16 -0300

    first comment

---
+++ index.js
@@ -0,0 +1,32 @@
+ // Crie um array que receba 5 itens e exiba no console.
+
+let item = ['Renan', 'Felipe', 'Ana', 'João', 'Maria']
+console.log(item)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+item.unshift('Juca')
+
+
+// Utilize um método para remover o último nome do array.
+
+item.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+item.push('Laura', 'Beatriz')
+
+// Utilize um método para remover o primeiro nome do array.
+
+item.shift()
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
+
+
